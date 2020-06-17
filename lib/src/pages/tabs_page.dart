cfec6ae288f54ebee32fbce82374975bc5b8ec84
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-06-16 23:59:09 -0500

    adding provider and navigator

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          Container(color: Colors.grey),
-          Container(color: Colors.teal),
-        ],
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => _NavigatorModel(),
+      child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _Navigator(),
       ),
     );
+  }
+}
+
+class _Navigator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final navigationModel = Provider.of<_NavigatorModel>(context);
+    return BottomNavigationBar(
+      currentIndex: navigationModel.actualPage,
+      onTap: (index) => navigationModel.actualPage = index,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), title: Text('Para ti')),
+        BottomNavigationBarItem(icon: Icon(Icons.public), title: Text('Encabezados')),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(color: Colors.grey),
+        Container(color: Colors.teal),
+      ],
+    );
+  }
+}
+
+class _NavigatorModel with ChangeNotifier {
+  num _actualPage = 0;
+
+  num get actualPage => this._actualPage;
+  set actualPage(num index) {
+    this._actualPage = index;
+    notifyListeners();
   }
 }
