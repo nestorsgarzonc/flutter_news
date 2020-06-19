e77029c08a1f8baffde34afa1a7ef4c167f7c4b8
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-06-19 16:30:52 -0500

    refactoring categoryButton

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/src/models/category_model.dart';
 import 'package:flutter_news/src/services/news_services.dart';
 import 'package:provider/provider.dart';
 
@@ -26,17 +27,46 @@ class _ListaCategorias extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: categories.length,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Icon(categories[index].icon),
-              SizedBox(height: 5),
-              Text(categories[index].name),
-            ],
+        return Container(
+          width: 105,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                _CategoryButton(categorie: categories[index]),
+                SizedBox(height: 5),
+                Text(
+                  "${categories[index].name[0].toUpperCase()}${categories[index].name.substring(1)}",
+                ),
+              ],
+            ),
           ),
         );
       },
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  const _CategoryButton({@required this.categorie});
+
+  final Category categorie;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('${categorie.name}');
+      },
+      child: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+        ),
+        child: Icon(categorie.icon, color: Colors.black54),
+      ),
     );
   }
 }
